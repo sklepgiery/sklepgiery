894ef32982febc1ffd5d64acbe9aa8e1029087ae
sklepgiery <47981270+sklepgiery@users.noreply.github.com>
2019-03-18 14:40:47 +0100

    bootsrap i inne w panelu

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -3,11 +3,64 @@
 @section('title', 'Wszystko')
 
 @section('content')
-    <a href="{{ action('Dashboard\Producers\ProducerController@index') }}">Producenci!</a>
-    <br />
-    <a href="{{ action('Dashboard\Genres\GenreController@index') }}">Gatunki!</a>
-    <br />
-    <a href="{{ action('Dashboard\Games\GameController@index') }}">Giery!</a>
-    <br />
-    <a href="{{ action('Dashboard\Discounts\DiscountController@index') }}">Kody rabatowe!</a>
+<div class="categories m-2">
+    <div class="row mb-2">
+        <div class="col-sm-4">
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">Producenci</h5>
+                    <p class="card-text">ilość Producentów: #</p>
+                    <a href="{{ action('Dashboard\Producers\ProducerController@index') }}" class="btn btn-primary">Przejdz do producentów</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm-4">
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">Giery</h5>
+                    <p class="card-text">ilość gier: #</p>
+                    <a href="{{ action('Dashboard\Games\GameController@index') }}" class="btn btn-primary">Przejdz do gier</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm-4">
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">Gatunki</h5>
+                    <p class="card-text">ilość gatunkow: #</p>
+                    <a href="{{ action('Dashboard\Genres\GenreController@index') }}" class="btn btn-primary">Przejdz do gatunkow</a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-sm-4">
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">Kody Rabatowe</h5>
+                    <p class="card-text">ilość Kodów: #</p>
+                    <a href="{{ action('Dashboard\Discounts\DiscountController@index') }}" class="btn btn-primary">Przejdz do kodów rabatowych</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm-4">
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">Giery</h5>
+                    <p class="card-text">ilość gier: #</p>
+                    <a href="#" class="btn btn-primary">Przejdz do gier</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm-4">
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">Gatunki</h5>
+                    <p class="card-text">ilość gatunkow: #</p>
+                    <a href="#" class="btn btn-primary">Przejdz do gatunkow</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 @endsection
